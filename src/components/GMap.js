5014de9717251e6ed06586e6a914554e2b6cceb4
Ethan Louw <160718170+eclouw@users.noreply.github.com>
2024-08-30 07:31:45 +0200

    Map latitude and longitude updated so it starts centered on Africa.
    Reverted a change to Package file to no longer use vite test server

--- src/components/GMap.js
+++ src/components/GMap.js
@@ -9,8 +9,8 @@ const GMap = () =>{
       <div className='map-container'>
         <APIProvider apiKey={'AIzaSyAZtWQe5IPX-2zR01ANR-y-WCvjMi9uOdQ'} onLoad={() => console.log('Maps API has loaded.')}>
         <Map
-          defaultZoom={13}
-          defaultCenter={ { lat: -33.860664, lng: 151.208138 } }
+          defaultZoom={2}
+          defaultCenter={ { lat: -0.00665793088812781, lng: 17.856514443765818 } }
           onCameraChanged={ (ev: MapCameraChangedEvent) =>
           console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
           }>
