a2a16efe95a1514c8d4e5084b87831bdd7069172
LWXETH001 <lwxeth001@myuct.ac.za>
2024-09-16 16:41:33 +0200

    Added styles for the MLAB Page

--- src/components/Pages/PageRoutes.js
+++ src/components/Pages/PageRoutes.js
@@ -1,10 +1,10 @@
 import NavBar from "../NavBar";
-import PageMLAB from "./PageMLAB.tsx";
+import PageMLAB from "./PageMLAB";
 import PageGoogleMaps from "./PageGoogleMaps";
 import PageHome from "./PageHome";
 import { Route, Routes } from "react-router-dom";
 import App from "../../App";
-import LeafletTest from "../../LeafletTest.js";
+import './styles.css'
 
 //Manages all the routing between pages
 //The route to app below is just for testing things
